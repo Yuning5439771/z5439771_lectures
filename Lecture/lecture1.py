6dcf090a682633a1c9295556fabc28821a20940f
Yuning Liu <z5439771@ad.unsw.edu.au>
2023-04-06 23:29:19 +1000

    try commit comment and push

--- Lecture/lecture1.py
+++ Lecture/lecture1.py
@@ -11,3 +11,5 @@ end = None                                                # (4)
 df = yfinance.download(tic, start, end, ignore_tz=True)   # (5)
 print(df)                                                 # (6)
 df.to_csv('qan_stk_prc.csv')                              # (7)
+
+# try commit comments
